e113a866b54a1e72ba47e8acb5cae7ff1b7ba775
Iliannymza <mendoza71997@gmail.com>
2025-06-09 16:27:10 +0200

    Initial Commit

---
+++ BMICalculadoraIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculadoraIOS
+//
+//  Created by Tardes on 9/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
